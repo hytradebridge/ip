b69678e9fbca4bde8fe57bfb40c236f976c2f063
huangjc <huangjc@hytradebridge.com>
2026-08-28 11:31:52 +0800

    Change banner, rename the chatbot, and add greeting and exit command

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Axiom User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Axiom.java
@@ -0,0 +1,16 @@
+public class Axiom {
+    public static void main(String[] args) {
+        String banner = "     _    __  _____ ___  __  __ \n"
+                      + "    / \\   \\ \\/ /_ _/ _ \\|  \\/  |\n"
+                      + "   / _ \\   \\  / | | | | | |\\/| |\n"
+                      + "  / ___ \\  /  \\ | | |_| | |  | |\n"
+                      + " /_/   \\_\\/_/\\_\\___\\___/|_|  |_|\n";
+        System.out.println("____________________________________________________________\n");
+        System.out.println(banner);
+        System.out.println("Hello! I'm AXIOM.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________\n");
+    }
+}
