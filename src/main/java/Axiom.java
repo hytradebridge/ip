0b9032fdbd2be62561aad88e21d2c19cc7529161
huangjc <huangjc@hytradebridge.com>
2026-08-28 11:36:02 +0800

    Implement echo and bye exit command

--- src/main/java/Axiom.java
+++ src/main/java/Axiom.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Axiom {
     public static void main(String[] args) {
         String banner = "     _    __  _____ ___  __  __ \n"
@@ -5,12 +7,22 @@ public class Axiom {
                       + "   / _ \\   \\  / | | | | | |\\/| |\n"
                       + "  / ___ \\  /  \\ | | |_| | |  | |\n"
                       + " /_/   \\_\\/_/\\_\\___\\___/|_|  |_|\n";
-        System.out.println("____________________________________________________________\n");
         System.out.println(banner);
         System.out.println("Hello! I'm AXIOM.");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________\n");
+        System.out.println("____________________________________________________________");
+
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String input = in.nextLine();
+            System.out.println("____________________________________________________________");
+            if (input.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
